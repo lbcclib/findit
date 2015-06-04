988cc202e5dd12e1310e199c0a248c518b61dc69
Jane Sandberg <sandbej@linnbenton.edu>
2015-06-03 21:47:15 -0400

    adding cover images to single record view

--- app/helpers/lbcc_helper.rb
+++ app/helpers/lbcc_helper.rb
@@ -97,11 +97,11 @@ module LbccHelper
         return ''
     end 
 
-    def display_representative_image(document)
-       return image_tag(select_representative_image(document), size:'60x30', alt:document['title_t'])
+    def display_representative_image(document, full_size=false )
+       return image_tag(select_representative_image(document, full_size), alt:document['title_t'], class:(full_size ? 'large-cover-image' : 'thumbnail-cover-image'))
     end
 
-    def select_representative_image(document)
+    def select_representative_image(document, full_size=false)
        if document.has? 'isbn_t'
           isbns = []
           case document['isbn_t']
@@ -116,7 +116,11 @@ module LbccHelper
              book = view.find_by_isbn(isbn)
              unless book.nil?
                 unless '' == book.thumbnail_url
-                   return book.thumbnail_url
+                   unless full_size
+                      return book.thumbnail_url
+                   else
+                      return book.thumbnail_url.sub('-S.jpg', '-L.jpg')
+                   end
                 end
              end
           end
