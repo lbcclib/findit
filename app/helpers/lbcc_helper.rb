66bd4e4071c33e4dc447c6d0e06cc9d287fd90df
unknown <libwebadmin@ALB-LIBWEB.lbcc.linnbenton.edu>
2015-11-24 09:12:15 -0800

    separating citations into their own helper

--- app/helpers/lbcc_helper.rb
+++ app/helpers/lbcc_helper.rb
@@ -10,27 +10,6 @@ module LbccHelper
     end
 
 
-    def create_bibtex(document)
-       bibtex = '@book{resource, '
-       if document.has? 'author_display'
-          bibtex.concat('author = {' + document['author_display'].gsub(/[0-9\-]/, '') + '},')
-       end
-       bibtex.concat('title = {' + document['title_display'] + '}')
-       if document.has? 'pub_date'
-          bibtex.concat(', year = ' + document['pub_date'][0])
-       end
-       if document.has? 'publisher_display'
-          if document['publisher_display'].is_a?(Array)
-             bibtex.concat(', publisher = {' + document['publisher_display'][0] + '}')
-          else
-             bibtex.concat(', publisher = ' + document['publisher_display'].to_s + '}')
-          end
-       end
-       bibtex.concat('}')
-       return bibtex
-    end
-
-    
     def display_access_options(document, context)
     # Context options: show (individual record), index (search results)
         style = ('index' == context) ? 'simple' : 'fancy'
@@ -41,42 +20,6 @@ module LbccHelper
             url_value = render_index_field_value(:document => document, :field => 'url_fulltext_display')
             display_fulltext_access_link(url_value, style)
         end
-    end
-
-    def generate_citations(document)
-       if document.has? 'bibtex_t'
-         begin
-           if document['bibtex_t'].is_a?(Array)
-             b = BibTeX.parse(document['bibtex_t'][0])
-           elsif document['bibtex_t'].is_a?(String)
-             b = BibTeX.parse(document['bibtex_t'])
-           else
-             # this is weird, because is_a?(String) or respond_to?(to_str) aren't working
-             b = BibTeX.parse(document['bibtex_t'].to_s)
-	   end
-	 rescue BibTeX::ParseError
-           b = BibTeX.parse(create_bibtex(document))
-	 end
-       else
-          b = BibTeX.parse(create_bibtex(document))
-       end
-       styles = Hash.new
-       citations = Hash.new
-       styles['APA'] = CiteProc::Processor.new format: 'html', style: 'apa'
-       styles['MLA'] = CiteProc::Processor.new format: 'html', style: 'modern-language-association'
-       styles['Chicago'] = CiteProc::Processor.new format: 'html', style: 'chicago-fullnote-bibliography'
-       styles['IEEE'] = CiteProc::Processor.new format: 'html', style: 'ieee'
-       styles['CBE'] = CiteProc::Processor.new format: 'html', style: 'council-of-science-editors'
-       styles.each do |shortname, processor|
-          processor.import b.to_citeproc
-          unless processor.empty?
-             citations[shortname] = processor.render(:bibliography, id: 'resource').first.tr('{}', '')
-          else
-             #citations[shortname] = 'Citations not available at this time'
-             citations[shortname] = document['bibtex_t']
-          end
-       end
-       return citations
     end
 
 
