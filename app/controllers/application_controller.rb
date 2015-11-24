65862989af7fddeb7305c11c87fa87f44e347ee0
unknown <libwebadmin@ALB-LIBWEB.lbcc.linnbenton.edu>
2015-11-23 16:42:41 -0800

    adding ahoy automatic tracking of all events

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  # Adds a few additional behaviors into the application controller 
+  include Blacklight::Controller
+  layout 'blacklight'
+
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  after_filter :track_action
+
+  protected
+
+  def track_action
+    ahoy.track "Processed #{controller_name}##{action_name}", request.filtered_parameters
+  end
+  
+end
