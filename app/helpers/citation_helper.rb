f05377b52563f9ea0e2f0c0d633868e6e1ff0de2
Jane Sandberg <sandbergja@users.noreply.github.com>
2015-11-25 08:27:25 -0800

    Updating documentation; helps with #67

--- app/helpers/citation_helper.rb
+++ app/helpers/citation_helper.rb
@@ -2,9 +2,9 @@ module CitationHelper
     include BlacklightHelper
     include ActionView::Helpers::TextHelper
 
-
+    # Creates a minimal BibTeX string in case the app can't find one
+    # in the data from Solr.
     def create_bibtex(document)
-    # A backup for creating a brief BibTeX string in case it wasn't added in the indexing step
        bibtex = '@book{resource, '
        if document.has? 'author_display'
           bibtex.concat('author = {' + document['author_display'].gsub(/[0-9\-]/, '') + '},')
@@ -24,7 +24,8 @@ module CitationHelper
        return bibtex
     end
 
-    
+    # Generates a hash containing properly formatted HTML citations
+    # using CiteProc based on BibTeX data found in a solr index.
     def generate_citations(document)
        if document.has? 'bibtex_t'
          begin
@@ -35,27 +36,43 @@ module CitationHelper
            else
              b = BibTeX.parse(document['bibtex_t'].to_s)
 	   end
+	 # Creates a minimal BibTeX string from solr data if
+	 # the official BibTeX string can't be parsed.
 	 rescue BibTeX::ParseError
-	 #If parsing the text from index time absolutely doesn't work, create a new BibTeX string on the fly
            b = BibTeX.parse(create_bibtex(document))
 	 end
        else
-          b = BibTeX.parse(create_bibtex(document))
+	 # Creates a minimal BibTeX string from solr data if
+	 # there is no BibTeX in the solr document.
+         b = BibTeX.parse(create_bibtex(document))
        end
+       
        styles = Hash.new
        citations = Hash.new
+       
+       # To add a new citation style:
+       # * the hash key will display in Find It as the label
+       #   for your citation style
+       # * the style value is the name of the citation style
+       #   as listed at https://github.com/citation-style-language/styles
        styles['APA'] = CiteProc::Processor.new format: 'html', style: 'apa'
        styles['MLA'] = CiteProc::Processor.new format: 'html', style: 'modern-language-association'
        styles['Chicago'] = CiteProc::Processor.new format: 'html', style: 'chicago-fullnote-bibliography'
        styles['IEEE'] = CiteProc::Processor.new format: 'html', style: 'ieee'
        styles['CBE'] = CiteProc::Processor.new format: 'html', style: 'council-of-science-editors'
+       
+       # Generates the actual HTML citation using data from the
+       # variable 'b' (our BibTeX string) and using the styles
+       # established in the hash 'styles.'
        styles.each do |shortname, processor|
           processor.import b.to_citeproc
           unless processor.empty?
              citations[shortname] = processor.render(:bibliography, id: 'resource').first.tr('{}', '')
           else
-             #citations[shortname] = 'Citations not available at this time'
-             citations[shortname] = document['bibtex_t']
+             # If CSL mysteriously can't parse the citation,
+             # just return the metadata from the variable 'b'
+             # (our BibTeX string.)
+             citations[shortname] = b
           end
        end
        return citations
