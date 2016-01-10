1b076e8eb5a9d41efb8ef876f4ef9bd05890a5bb
LBCC Library <libref@linnbenton.edu>
2016-01-10 10:36:31 -0800

    changing more facets to sort by count

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -70,9 +70,9 @@ class CatalogController < ApplicationController
     config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
     config.add_facet_field 'language_facet', :label => 'Language', :limit => true, :sort => 'count'
     config.add_facet_field 'lc_1letter_facet', :label => 'Call number' 
-    config.add_facet_field 'subject_geo_facet', :label => 'Region of focus', :limit => true
-    config.add_facet_field 'subject_era_facet', :label => 'Era of focus', :limit => true
-    config.add_facet_field 'subject_name_facet', :label => 'People and groups', :limit => true
+    config.add_facet_field 'subject_geo_facet', :label => 'Region of focus', :limit => true, :sort => 'count'
+    config.add_facet_field 'subject_era_facet', :label => 'Era of focus', :limit => true, :sort => 'count'
+    config.add_facet_field 'subject_name_facet', :label => 'People and groups', :limit => true, :sort => 'count'
     config.add_facet_field 'record_source_facet', :label => 'Source database', :limit => true
 
     config.advanced_search = {
