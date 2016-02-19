97eb119b369fd6d54687f04f03e3a0ed07b2d574
Jane Sandberg <sandbej@linnbenton.edu>
2016-02-18 21:30:26 -0800

    Rewriting display_field method to make RDFa more granular; closes #50

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -18,37 +18,43 @@ require 'uri'
     def display_field(document, field_name, label, opts = {})
         
         # Make sure that the document actually has the
-        # field we want
+        # desired field
         if document.has? field_name
             values = field_to_array(document[field_name])
         else
             return nil
         end
 
-        # Add a DC RDFa attribute
-        if opts.key?(:dc_element)
-           dc_property_string = opts[:dc_element].length > 0 ? 'property="http://purl.org/dc/terms/' + opts[:dc_element] + '"' : opts[:dc_element] 
+        values_html = Array.new
+
+        values.each do |value|
+           # Display this field as a link to a search
+           if opts[:search_link]
+              # If no search field is specified, default to
+              # searching all fields
+              search_field = opts.key?(:search_fields) ? opts[:search_fields] : 'all_fields'
+              exact_match = opts.key?(:exact_match) ? opts[:exact_match] : false
+              value_string = search_catalog_link(strip(value), search_field, exact_match)
+
+           elsif opts[:contains_url]
+              value_string = external_link(value) 
+
+           else
+              value_string = value
+           end
+
+           # Add a DC RDFa attribute
+           if opts.key?(:dc_element)
+              entry = opts[:dc_element].length > 0 ? '<span property="http://purl.org/dc/terms/' + opts[:dc_element] + '">' + value_string + '</span>' : value_string
+           else
+              entry = value_string
+           end
+        values_html.push(entry)
         end
 
-        # Display this field as a link to a search
-        if opts[:search_link]
-            # If no search field is specified, default to
-            # searching all fields
-            search_field = opts.key?(:search_fields) ? opts[:search_fields] : 'all_fields'
-            exact_match = opts.key?(:exact_match) ? opts[:exact_match] : false
-            value_string = values.collect{ |value| search_catalog_link(strip(value), search_field, exact_match)}.join('; ')
-        elsif opts[:contains_url]
-            value_string = values.collect{ |url| external_link(url)}.join('; ')
-        else
-            value_string = values.collect{ |value| strip(value)}.join('; ')
-        end
-
-        if values.any?
-            display_string  = <<-EOS
-             <dt>#{label}:</dt>
-             <dd #{dc_property_string}>#{value_string}</dd>
-            EOS
-            return display_string.html_safe
+        if values_html.any?
+           display_string = '<dt>' + label + '</dt><dd>' + values_html.join('; ')
+           return display_string.html_safe
         end
     end
 
