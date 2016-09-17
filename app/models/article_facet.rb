a7d37ea2e656bc4c759ae202be5188012eb78882
Jane Sandberg <sandbergja@users.noreply.github.com>
2016-09-16 17:39:54 -0700

    documenting some methods

--- app/models/article_facet.rb
+++ app/models/article_facet.rb
@@ -1,11 +1,13 @@
 class ArticleFacet
     attr_reader :title, :values
 
+    # Create a new facet
     def initialize title
         @title = title
         @values = Array.new
     end
 
+    # Add a new value to an existing facet
     def add_value value, action, count
         tmp = Hash.new
         tmp[:value] = value
