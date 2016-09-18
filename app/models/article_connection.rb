ad65acbb9e7f2e8f9485300ec72f81c080426e7d
Jane Sandberg <sandbej@linnbenton.edu>
2016-09-18 10:11:17 -0700

    fixing class vs instance variables in article connection

--- app/models/article_connection.rb
+++ app/models/article_connection.rb
@@ -1,28 +1,28 @@
 class ArticleConnection
   # Create an initial connection to the API
   def initialize
-    @raw_connection = :eds_connection
+    @raw_connection = :@eds_connection
     @auth_method = :ip
     if using_eds?
-      @@raw_connection = EDSApi::ConnectionHandler.new(2)
+      @eds_connection = EDSApi::ConnectionHandler.new(2)
       if using_uid_auth?
         @auth_method = :uid
-        @@raw_connection.uid_init(
+        @eds_connection.uid_init(
           Rails.configuration.articles['username'],
           Rails.configuration.articles['password'],
           'edsapi')
-        @@raw_connection.uid_authenticate
+        @eds_connection.uid_authenticate
       else
-        @@raw_connection.ip_init 'edsapi'
-        @@raw_connection.ip_authenticate
+        @eds_connection.ip_init 'edsapi'
+        @eds_connection.ip_authenticate
       end
-      @@raw_connection.create_session
+      @eds_connection.create_session
     end
   end
 
   def ready?
-    if :eds_connection == @raw_connection
-      if @@raw_connection.show_session_token and @@raw_connection.show_auth_token
+    if :@eds_connection == @raw_connection
+      if @eds_connection.show_session_token and @eds_connection.show_auth_token
         return true
       end
       return false
@@ -30,27 +30,27 @@ class ArticleConnection
   end
 
   def retrieve_single_article db, id
-    if :eds_connection == @raw_connection
+    if :@eds_connection == @raw_connection
       if @auth_method = :uid
-        return @@raw_connection.retrieve db, id, '', '', @@raw_connection.show_session_token, @@raw_connection.show_auth_token
+        return @eds_connection.retrieve db, id, '', '', @eds_connection.show_session_token, @eds_connection.show_auth_token
       else
-        return @@raw_connection.retrieve db, id
+        return @eds_connection.retrieve db, id
       end
     end
   end
 
   def send_search search_opts
-    if :eds_connection == @raw_connection
+    if :@eds_connection == @raw_connection
       if @auth_method = :uid
         begin
-          return @@raw_connection.search search_opts, @@raw_connection.show_session_token, @@raw_connection.show_auth_token
+          return @eds_connection.search search_opts, @eds_connection.show_session_token, @eds_connection.show_auth_token
         rescue ActionView::Template::Error, Net::ReadTimeout
           logger.debug "EDS timed out"
           return false
         end
       else
         begin
-          return @@raw_connection.search search_opts
+          return @eds_connection.search search_opts
         rescue ActionView::Template::Error, Net::ReadTimeout
           logger.debug "EDS timed out"
           return false
