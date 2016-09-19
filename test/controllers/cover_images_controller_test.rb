a56a0f7c96d4ca7e4de240fdeced1e7b0270227d
Jane Sandberg <sandbej@linnbenton.edu>
2016-09-18 23:07:50 -0700

    adding tests

--- test/controllers/cover_images_controller_test.rb
+++ test/controllers/cover_images_controller_test.rb
@@ -6,5 +6,12 @@ class CoverImagesControllerTest < ActionController::TestCase
         identifiers = extract_identifiers SolrDocument.new, 'dsjfsldjflsdkfjsdlkfjsdlkfjdslkfjsdlfkjsdlkfjsldkfjlskdfs'
         assert_instance_of Array, identifiers
     end
+    test "Trying to extract identifiers from a singleton solr field returns an array" do
+        document = SolrDocument.new
+        document['isbn_t'] = '9781595581037'
+        identifiers = extract_identifiers document, 'isbn_t'
+        assert_instance_of Array, identifiers
+        assert_equal 1, identifiers.size
+    end
 end
 
