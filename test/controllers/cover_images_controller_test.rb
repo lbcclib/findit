bc251c58efeee24d959d36400ecea8f0c4a0b089
Jane Sandberg <sandbej@linnbenton.edu>
2016-09-18 22:43:29 -0700

    adding test for cover images controller

--- test/controllers/cover_images_controller_test.rb
+++ test/controllers/cover_images_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
+include CoverImagesController
 
 class CoverImagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "Trying to extract identifiers from a non-existant solr field returns a blank array" do
+        identifiers = extract_identifiers SolrDocument.new, 'dsjfsldjflsdkfjsdlkfjsdlkfjdslkfjsdlfkjsdlkfjsldkfjlskdfs'
+        assert_instance_of Array, identifiers
+    end
 end
+
