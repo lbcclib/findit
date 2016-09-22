32631610b8b8c5b0b7d5196a7fec9863c0343000
Jane Sandberg <sandbej@linnbenton.edu>
2016-09-22 09:48:22 -0700

    adding citation test

--- test/helpers/citation_helper_test.rb
+++ test/helpers/citation_helper_test.rb
@@ -1,44 +1,64 @@
 require 'test_helper'
 
 class CitationHelperTest < ActionView::TestCase
+
+  setup :initialize_vars
+
   test "Test that citations are being returned with 1 element in parseable HTML" do
-
-    document = SolrDocument.new
-    document['title_display'] = 'How to eat pudding correctly'
-
-    citations_generated = generate_citations(document)
+    citations_generated = generate_citations(@short_document)
     citations_generated.each do |val|
-    # puts val.class
-    assert Nokogiri::HTML.parse(val[1])
-    end
-  end
-
-    test "Test that citations are being returned with 4 elements in parseable HTML" do
-
-      document = SolrDocument.new
-      # document['author_display'] = 'David Cluckner'
-      # document['title_display'] = 'Chickens are Good People'
-      #document['pub_date'] = Array.new('1994')
-      # document['publisher_display'] = Array.new('National Chicken Books of Awesome')
-
-      def book_title
-        'The ' + Faker::Pokemon.name + ' from ' + Faker::Pokemon.location
-      end
-
-      def pub_comp
-        'House ' + Faker::GameOfThrones.house + ' Publishing'
-      end
-
-      # Faker test
-      document['title_display'] = book_title
-      document['author_display'] = Faker::GameOfThrones.character
-      document['pub_date'] = Array(Faker::Number.number(4).to_s)
-      document['publisher_display'] = Array(pub_comp)
-      puts  document['title_display'], document['author_display'], document['pub_date'], document['publisher_display']
-
-      citations_generated = generate_citations(document)
-      citations_generated.each do |val|
       assert Nokogiri::HTML.parse(val[1])
     end
   end
+
+  test "Test that citations are being returned with 4 elements in parseable HTML" do
+    citations_generated = generate_citations(@long_document)
+    citations_generated.each do |val|
+      assert Nokogiri::HTML.parse(val[1])
+    end
+  end
+
+  test "Test that citations are generated for the correct documents" do
+    words_in_citations = []
+    citations_generated = generate_citations(@long_document)
+    citations_generated.each do |val|
+      words = val[1].split
+      words.each do |word|
+        if word.length > 3
+          words_in_citations.push strip_tags(word).downcase.gsub /[[:punct:]]/, ''
+        end
+      end
+    end
+
+    words_in_document = ['print', 'online']
+    @words_in_long_document.each do |words|
+      words.split.each do |word|
+        words_in_document.push word.downcase.gsub /[[:punct:]]/, ''
+      end
+    end
+    words_in_citations.each do |word|
+      assert words_in_document.include? word
+    end
+  end
+
+
+protected
+  def initialize_vars
+    @book_title = 'The ' + Faker::Pokemon.name + ' from ' + Faker::Pokemon.location
+    @publisher = 'House ' + Faker::GameOfThrones.house + ' Publishing'
+    @author = Faker::GameOfThrones.character
+    @pub_date = Faker::Number.number(4).to_s
+
+    @short_document = SolrDocument.new
+    @short_document['title_display'] = 'How to eat pudding correctly'
+
+    @long_document = SolrDocument.new
+    @long_document['title_display'] = @book_title
+    @long_document['author_display'] = @author
+    @long_document['pub_date'] = Array(@pub_date)
+    @long_document['publisher_display'] = Array(@pub_comp)
+
+    @words_in_long_document = [@book_title, @publisher, @author, @pub_date]
+  end
+
 end
