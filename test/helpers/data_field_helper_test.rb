036cfb7d52de411c7ce6ee3884de7f942c1b43dd
Jane Sandberg <sandbej@linnbenton.edu>
2016-09-30 14:16:25 -0700

    re-organizing the array tests

--- test/helpers/data_field_helper_test.rb
+++ test/helpers/data_field_helper_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class DataFieldHelperTest < ActionView::TestCase
-  puts Dir.pwd
   data_strings = YAML::load_file Rails.root.join('test', 'fixtures', 'data_strings.yml')
+
+  test "Convert a string into an array" do
+    assert_kind_of Array, field_to_array("A Magical String")
+  end
+
+  test "Convert an int into an array" do
+    assert_kind_of Array, field_to_array(283)
+  end
+
+  test "Convert a Boolean into an array" do
+    assert_kind_of Array, field_to_array(True)
+  end
+
 
   test "Snippet produces text that is no longer than 200 characters using less than 200 characters" do
     assert snippet({Value: data_strings[:short_string]}).length <200
