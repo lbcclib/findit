1a1ff08e0366b332303a69a13a56be08a21de05b
Jane Sandberg <sandbej@linnbenton.edu>
2017-03-12 08:45:00 -0700

    adding eds authentication test, helps with #149

--- test/models/eds_connection_test.rb
+++ test/models/eds_connection_test.rb
@@ -6,4 +6,10 @@ class EdsConnectionTest < ActiveSupport::TestCase
         conn = EdsConnection.new
         assert_not conn.using_uid_auth?
     end
+    test "When EDS Username and password are both available in Rails.configuration, the app will attempt UID authentication" do
+        Rails.configuration.articles['username'] = 'username'
+        Rails.configuration.articles['password'] = 'password'
+        conn = EdsConnection.new
+        assert conn.using_uid_auth?
+    end
 end
