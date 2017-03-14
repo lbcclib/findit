124d87013a2626ef7ee59fd65dccf44aabee839b
Jane Sandberg <sandbej@linnbenton.edu>
2017-03-13 22:21:35 -0700

    DRYing out the EDS test a little

--- test/models/eds_connection_test.rb
+++ test/models/eds_connection_test.rb
@@ -1,10 +1,12 @@
 require 'test_helper'
 
 class EdsConnectionTest < ActiveSupport::TestCase
+    setup :initialize_vars
+
     test "When EDS Username is nil, the app will attempt IP authentication" do
         Rails.configuration.articles['username'] = nil
-        conn = EdsConnection.new
-        assert_not conn.using_uid_auth?
+        @ip_conn = EdsConnection.new
+        assert_not @ip_conn.using_uid_auth?
     end
     test "When EDS Username and password are both available in Rails.configuration, the app will attempt UID authentication" do
         Rails.configuration.articles['username'] = 'username'
@@ -12,4 +14,13 @@ class EdsConnectionTest < ActiveSupport::TestCase
         conn = EdsConnection.new
         assert conn.using_uid_auth?
     end
+
+
+    private
+    def initialize_vars
+        Rails.configuration.articles['username'] = nil
+        @ip_conn = EdsConnection.new
+    end
+
+
 end
