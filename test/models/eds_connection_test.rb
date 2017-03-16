7f8fb84cbd894150fe3b4861da0081490603ea07
Jane Sandberg <sandbej@linnbenton.edu>
2017-03-15 20:58:12 -0700

    Adding tests for EDS connections, helps with #161 and #149

--- test/models/eds_connection_test.rb
+++ test/models/eds_connection_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
+require 'securerandom'
+require 'pp'
+
+class TestConnectionHandler < EDSApi::ConnectionHandler
+    def initialize
+        @session_token = SecureRandom.hex
+        @auth_token = SecureRandom.hex
+    end
+end
+
+class TestEdsConnection < EdsConnection
+    def initialize
+        super
+        @raw_connection = TestConnectionHandler.new
+    end
+end
 
 class EdsConnectionTest < ActiveSupport::TestCase
     setup :initialize_vars
@@ -8,6 +24,7 @@ class EdsConnectionTest < ActiveSupport::TestCase
         @ip_conn = EdsConnection.new
         assert_not @ip_conn.using_uid_auth?
     end
+
     test "When EDS Username and password are both available in Rails.configuration, the app will attempt UID authentication" do
         Rails.configuration.articles['username'] = 'username'
         Rails.configuration.articles['password'] = 'password'
@@ -15,6 +32,33 @@ class EdsConnectionTest < ActiveSupport::TestCase
         assert conn.using_uid_auth?
     end
 
+    test "When EDS password is unavailable in Rails.configuration, the app will attempt IP authentication" do
+        Rails.configuration.articles['username'] = 'username'
+        Rails.configuration.articles.delete('password')
+        conn = EdsConnection.new
+        assert_not conn.using_uid_auth?
+    end
+
+    test "When EDS username is unavailable in Rails.configuration, the app will attempt IP authentication" do
+        Rails.configuration.articles.delete('username')
+        Rails.configuration.articles['password'] = 'password'
+        conn = EdsConnection.new
+        assert_not conn.using_uid_auth?
+    end
+
+    test "UID-authenticated EDS Connection reports that it is ready when both session token and auth token are available" do
+        Rails.configuration.articles['username'] = 'username'
+        Rails.configuration.articles['password'] = 'password'
+        conn = TestEdsConnection.new
+        assert conn.ready?
+    end
+
+    test "IP-authenticated EDS Connection reports that it is ready when both session token and auth token are available" do
+        Rails.configuration.articles.delete('username')
+        Rails.configuration.articles['password'] = 'password'
+        conn = TestEdsConnection.new
+        assert conn.ready?
+    end
 
     private
     def initialize_vars
