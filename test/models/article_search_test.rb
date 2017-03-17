e6ae8731392fbeaeac0db55aa04bfb356c17044d
Jane Sandberg <sandbej@linnbenton.edu>
2017-03-17 15:44:25 -0700

    tests and documentation for ArticleSearch model

--- test/models/article_search_test.rb
+++ test/models/article_search_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
+
 class ArticleSearchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Article Search search_opts method returns an array" do
+    a = ArticleSearch.new 'soap opera', 3, [], nil
+    assert_instance_of Array, a.search_opts
+  end
+
 end
