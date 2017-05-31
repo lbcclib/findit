e57c92976023717c9dcdc3ee11b7e98e772d6bb6
Jarred Muscutt <jarred.muscutt.4136@mail.linnbenton.edu>
2017-05-31 18:57:13 +0000

    merge

--- test/helpers/data_field_helper_test.rb
+++ test/helpers/data_field_helper_test.rb
@@ -20,15 +20,15 @@ class DataFieldHelperTest < ActionView::TestCase
   end
 
   test "Snippet produces text that is no longer than 200 characters using less than 200 characters" do
-    assert snippet({Value: data_strings[:short_string]}).length <200
+    assert snippet({value: data_strings[:short_string]}).length <200
   end
 
     test "Snippet produces text that is no longer than 200 characters using exactly 200 characters" do
-      assert snippet({Value: data_strings[:string_with_200_chars]}).length <200
+      assert snippet({value: data_strings[:string_with_200_chars]}).length <200
     end
 
     test "Snippet produces text that is no longer than 200 characters using more than 200 characters" do
-      assert snippet({Value: data_strings[:very_long_string]}).length <200
+      assert snippet({value: data_strings[:very_long_string]}).length <200
     end
 
     test "external link produces valid HTML link when given a well-formatted URL" do
@@ -38,6 +38,7 @@ class DataFieldHelperTest < ActionView::TestCase
     test "external_link does not produce a link to any poorly-formatted URLs" do
       assert external_link 'http://library.linnbenton.edu/c.php?g=13287&p=2901925' !~ /<a\b[^>]*>(.*?)<\/a>/i
     end
+
     test "check that short document has a field_name" do
       assert get_array_from_solr @short_document, :title_display
     end
@@ -49,4 +50,11 @@ class DataFieldHelperTest < ActionView::TestCase
     test "check that long document does not have a field_name" do
       assert !(get_array_from_solr @long_document, :url_fulltext_display)
     end
+
+  test "Snippet selects first element in array if snippet is array" do
+    testarray = ["str1", "str2"]
+    assert_equal("str1", snippet({value: testarray }))
+  end
+
+
 end
