14d21338f6d91d48384e5617c73e9605c7ee8f2f
Jane Sandberg <sandbej@linnbenton.edu>
2017-09-09 10:54:32 -0700

    Article helper no longer needed

--- test/helpers/article_helper_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-require Rails.root.join('app', 'helpers', 'data_field_helper').to_s
-include DataFieldHelper
-
-class ArticleHelperTest < ActionView::TestCase
-    test "Article type is a string" do
-        assert_kind_of String, display_article_type('Journal')
-    end
-end
