56bd4d03b7ecb36e91e622952222454335f22b25
Jane Sandberg <sandbej@linnbenton.edu>
2017-10-27 04:42:03 -0700

    Now using ebsco-eds gem for index/show views of articles

--- app/models/eds_connection.rb
+++ app/models/eds_connection.rb
@@ -1,45 +1,27 @@
-require 'openssl'
 # Connect to an external API to get some articles
 class EdsConnection < ArticleConnection
   # Create an initial connection to the API
   def initialize
-    @auth_method = :ip
-    @raw_connection = EDSApi::ConnectionHandler.new(2)
-    if using_uid_auth?
-      @auth_method = :uid
-      @raw_connection.uid_init(
-        Rails.configuration.articles['username'],
-        Rails.configuration.articles['password'],
-        'edsapi')
-      @raw_connection.uid_authenticate
-      @last_authentication = Time.current
-    else
-      begin
-        @raw_connection.ip_init 'edsapi'
-        @raw_connection.ip_authenticate
-      rescue NoMethodError
-      end
-    end
-    @raw_connection.create_session
+    @raw_connection = EBSCO::EDS::Session.new({
+      :user => Rails.configuration.articles['username'],
+      :pass => Rails.configuration.articles['password'],
+      :profile => 'edsapi'
+    })
+    @last_authentication = Time.current
   end
 
   # Make sure the EDS connection has all it needs to send searches
   def ready?
-    if @raw_connection.show_session_token and @raw_connection.show_auth_token
-      if @auth_method == :uid 
-          if @last_authentication < 20.minutes.ago
-              initialize
-          end
-      end
-      return true
+    unless (@last_authentication and @last_authentication > 20.minutes.ago)
+      initialize
     end
-    return false
+    return true
   end
 
   # Fetch a single article from the external API
   def retrieve_single_article db, id
-      article_identifiers = {'dbid' => db, 'an' => id}
-      return @raw_connection.retrieve article_identifiers
+    article_identifiers = {:dbid => db, :an => id}
+    return @raw_connection.retrieve(article_identifiers)
   end
 
   # Send an entire search, and return the JSON data produced by
