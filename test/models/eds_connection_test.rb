4ed2799a9bde8cae933f48175c6eb7b31fc62e19
Jane Sandberg <sandbej@linnbenton.edu>
2017-11-08 10:13:26 -0600

    Rewriting tests for new EDS interface

--- test/models/eds_connection_test.rb
+++ test/models/eds_connection_test.rb
@@ -2,93 +2,25 @@ require 'test_helper'
 require 'securerandom'
 require 'pp'
 
-class TestConnectionHandler < EDSApi::ConnectionHandler
-    def initialize
-        @session_token = SecureRandom.hex
-        @auth_token = SecureRandom.hex
-    end
-end
-
-class TestEdsConnection < EdsConnection
-    attr_reader :raw_connection
-    attr_writer :last_authentication
-    def initialize
-        super
-        @raw_connection = TestConnectionHandler.new
-    end
-end
-
-class TestUnauthenticatedConnection < EdsConnection
-    attr_accessor :raw_connection
-    def initialize
-        @raw_connection = TestConnectionHandler.new
-        @raw_connection.auth_token = nil
-    end
-end
-
 class EdsConnectionTest < ActiveSupport::TestCase
     setup :initialize_vars
 
-    test "When EDS Username is nil, the app will attempt IP authentication" do
-        Rails.configuration.articles['username'] = nil
-        @ip_conn = EdsConnection.new
-        assert_not @ip_conn.using_uid_auth?
+    test "Can send 1000 searches without erroring" do
+	opts = {q: 'cats'}
+	(1..1000).each do
+	    @eds_conn.send_search opts
+	end
     end
 
-    test "When EDS Username and password are both available in Rails.configuration, the app will attempt UID authentication" do
-        Rails.configuration.articles['username'] = 'username'
-        Rails.configuration.articles['password'] = 'password'
-        conn = EdsConnection.new
-        assert conn.using_uid_auth?
-    end
-
-    test "When EDS password is unavailable in Rails.configuration, the app will attempt IP authentication" do
-        Rails.configuration.articles['username'] = 'username'
-        Rails.configuration.articles.delete('password')
-        conn = EdsConnection.new
-        assert_not conn.using_uid_auth?
-    end
-
-    test "When EDS username is unavailable in Rails.configuration, the app will attempt IP authentication" do
-        Rails.configuration.articles.delete('username')
-        Rails.configuration.articles['password'] = 'password'
-        conn = EdsConnection.new
-        assert_not conn.using_uid_auth?
-    end
-
-    test "UID-authenticated EDS Connection reports that it is ready when both session token and auth token are available" do
-        Rails.configuration.articles['username'] = 'username'
-        Rails.configuration.articles['password'] = 'password'
-        conn = TestEdsConnection.new
-        assert conn.ready?
-    end
-
-    test "UID-authenticated EDS Connection re-initializes if last_authentication was more than 20 minutes ago" do
-        Rails.configuration.articles['username'] = 'username'
-        Rails.configuration.articles['password'] = 'password'
-        conn = TestEdsConnection.new
-        first_session_token = conn.raw_connection.show_session_token
-        conn.last_authentication = 45.minutes.ago
-        conn.ready?
-        assert_not_equal first_session_token, conn.raw_connection.show_session_token
-    end
-
-    test "IP-authenticated EDS Connection reports that it is ready when both session token and auth token are available" do
-        Rails.configuration.articles.delete('username')
-        Rails.configuration.articles['password'] = 'password'
-        conn = TestEdsConnection.new
-        assert conn.ready?
-    end
-
-    test "When no appropriate tokens are available, the EDS Connection object does not report that it is ready" do
-        conn = TestUnauthenticatedConnection.new
-        assert_not conn.ready?
+    test "Can retrieve an article 1000 times without erroring" do
+	(1..10).each do
+	    @eds_conn.retrieve_single_article 'f5h', '124413323'
+	end
     end
 
     private
     def initialize_vars
-        Rails.configuration.articles['username'] = nil
-        @ip_conn = EdsConnection.new
+        @eds_conn = EdsConnection.new
     end
 
 
