84742911c51d083f576c9ce5fa13306e27bd9632
LBCC Library <libref@linnbenton.edu>
2018-08-01 14:18:22 -0700

    De-prioritizing Open Textbook Library

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -49,7 +49,7 @@ class CatalogController < ApplicationController
       ].join(' '),
       :rows => 10,
       :fl => '*',
-      :bq => 'is_electronic_facet:"Albany Campus Library"^250.0 "Healthcare Occupations Center"^175.0 Online^30.0 record_source_facet:"eBrary Academic Complete"^25.0 "NCBI Bookshelf"^35.0 "Open Textbook Library"^50.0 pub_date_sort:[2015 TO *]^10.0 [1923 TO *]^30.0',
+      :bq => 'is_electronic_facet:"Albany Campus Library"^250.0 "Healthcare Occupations Center"^175.0 Online^30.0 record_source_facet:"eBrary Academic Complete"^25.0 "NCBI Bookshelf"^35.0 pub_date_sort:[2015 TO *]^10.0 [1923 TO *]^30.0',
     }
     
     # solr path which will be added to solr base url before the other solr params.
