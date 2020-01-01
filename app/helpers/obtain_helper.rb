f9c72a32618fdc9aa0fa210c72bbce238141ac8a
Jane Sandberg <sandbej@linnbenton.edu>
2020-01-01 10:35:52 -0800

    Finishing evergreen catalog index partials

--- app/helpers/obtain_helper.rb
+++ app/helpers/obtain_helper.rb
@@ -5,4 +5,8 @@ module ObtainHelper
   def is_online_record? document
     return document.has? 'url_fulltext_display'
   end
+  def url_for_evergreen_hold tcn
+    return 'https://libcat.linnbenton.edu/eg/opac/place_hold?query=locg=8;detail_record_view=1;hold_target=' + Array.wrap(tcn).first.to_s + ';hold_type=T'
+  end
+
 end
