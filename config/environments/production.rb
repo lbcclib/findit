3dfc976d57bbe2ace3e57a39cbb23917c3a1a1b0
Jane Sandberg <sandbej@linnbenton.edu>
2020-01-03 13:48:29 -0800

    Improve asset compiling; helps with #234

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,10 +23,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
