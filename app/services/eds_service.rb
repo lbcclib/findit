4e913b48e56a17593e1acea842d441bdb267b415
Jane Sandberg <sandbej@linnbenton.edu>
2020-01-06 04:06:55 -0800

    Get rid of testing puts statements

--- app/services/eds_service.rb
+++ app/services/eds_service.rb
@@ -1,17 +1,8 @@
 class EdsService
   def self.get_valid_connection session
     if (session.key? 'eds_session_token') && (session.key? 'eds_session_timestamp') && (session['eds_session_timestamp'] > 15.minutes.ago)
-	    puts "IN HERE BUT NOT SUPPOSED TO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
       ::EBSCO::EDS::Session.new session_token: session['eds_session_token']
     else
-	    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&"
-	    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&"
-	    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&"
-	    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&"
-	    puts "GAVE ME BAD"
-	    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&"
-	    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&"
-	    puts "&&&&&&&&&&&&&&&&&&&&&&&&&&"
       ses = ::EBSCO::EDS::Session.new
       session['eds_session_token'] = ses.session_token
       session['eds_session_timestamp'] = Time.now
