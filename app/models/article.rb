bdbb30163b4ffb95fb5aeb142ff281b4a0d7751d
Jane Sandberg <sandbej@linnbenton.edu>
2020-01-15 18:21:48 -0800

    Fixing syntax error in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,6 +16,7 @@ class Article < SolrDocument
                         @_source[:url_fulltext_display] = link[:url]
                     else
                         @_source[:url_fulltext_display] = PROXY_PREFIX + link[:url]
+                    end
                     break
                 end
             end
@@ -28,20 +29,20 @@ class Article < SolrDocument
             @_source[:pubtype] = record.eds_publication_type
             @_source[:article_author_display] = record.eds_authors
             @_source[:article_language_facet] = record.eds_languages
-	        @_source[:article_subject_facet] = record.eds_subjects
+            @_source[:article_subject_facet] = record.eds_subjects
             @_source[:database_display] = record.eds_database_name
-	        @_source[:pub_date] = record.eds_publication_year
+            @_source[:pub_date] = record.eds_publication_year
 
-	        @_source[:journal_display] = try_to_extract record, :eds_source_title
-	        @_source[:abstract_display] = try_to_extract record, :eds_abstract
-	        @_source[:doi_display] = try_to_extract record, :eds_doi
-	        @_source[:page_count_display] = try_to_extract record, :eds_page_count
-	        @_source[:page_number_display] = try_to_extract record, :eds_page_start
-	        @_source[:publisher_info_display] = try_to_extract record, :eds_publication_info
-	        @_source[:thumbnail_url_display] = try_to_extract record, :eds_cover_thumb_url
-	        @_source[:volume_display] = try_to_extract record, :eds_volume
-	        @_source[:issue_display] = try_to_extract record, :eds_issue
-	        @_source[:notes_display] = try_to_extract record, :eds_notes
+            @_source[:journal_display] = try_to_extract record, :eds_source_title
+            @_source[:abstract_display] = try_to_extract record, :eds_abstract
+            @_source[:doi_display] = try_to_extract record, :eds_doi
+            @_source[:page_count_display] = try_to_extract record, :eds_page_count
+            @_source[:page_number_display] = try_to_extract record, :eds_page_start
+            @_source[:publisher_info_display] = try_to_extract record, :eds_publication_info
+            @_source[:thumbnail_url_display] = try_to_extract record, :eds_cover_thumb_url
+            @_source[:volume_display] = try_to_extract record, :eds_volume
+            @_source[:issue_display] = try_to_extract record, :eds_issue
+            @_source[:notes_display] = try_to_extract record, :eds_notes
         end
     end
 
