bc0346d36626a3a7939295cbe47e3dc333a94237
Jane Sandberg <sandbej@linnbenton.edu>
2020-05-30 19:15:07 -0700

    Fixing JoMI id creation, closes #313

--- lib/tasks/data/config/jomi.rb
+++ lib/tasks/data/config/jomi.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'traject'
 
 to_field 'record_provider_facet', literal('JoMI Surgical Videos')
@@ -5,9 +7,6 @@ to_field 'record_source_facet', literal('JoMI Surgical Videos')
 to_field 'is_electronic_facet', literal('Online')
 to_field 'format', literal('Streaming video')
 to_field 'id' do |record, accumulator|
-	field001s = record.find_all {|f| f.tag == '001'}
-	field001s.each do |field|
-		id = 'jomi' + field.value
-		accumulator = [id]
-	end
+  id = "jomi#{record['001'].value}"
+  accumulator << id
 end
