662168f9611cc5605179834f7c4d545b0f7b034c
Jane Sandberg <sandbej@linnbenton.edu>
2020-05-31 13:24:38 -0700

    Move some logic out of an article view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,13 @@
-class ArticlesController < CatalogController
+# frozen_string_literal: true
 
+class ArticlesController < CatalogController
   include BlacklightRangeLimit::ControllerOverride
 
   def index
     if has_search_parameters?
       page = params[:page].present? ? Integer(params[:page]) : 1
       q = params[:q] || 'Linn-Benton Community College'
-      search_fields = {'author' => 'AU:', 'title' =>  'TI:', 'all_fields' => '', 'subject' => 'SU:'}
+      search_fields = { 'author' => 'AU:', 'title' => 'TI:', 'all_fields' => '', 'subject' => 'SU:' }
       search_field = params[:search_field] || 'all_fields'
       search_field_code = search_fields[@search_field] || ''
       requested_facets = params[:f] || []
@@ -27,30 +28,32 @@ class ArticlesController < CatalogController
           results.facets.each do |facet|
             items = []
             facet[:values].take(10).each do |value|
-              items.push(OpenStruct.new hits: value[:hitcount], value: value[:action].gsub(/addfacetfilter\(\w+\:(.*)\)/, '\1').gsub(/\\(\(|\))/, '\1'), label: value[:value])
+              items.push(OpenStruct.new(hits: value[:hitcount], value: value[:action].gsub(/addfacetfilter\(\w+\:(.*)\)/, '\1').gsub(/\\(\(|\))/, '\1'), label: value[:value]))
             end
             new_facet = Blacklight::Solr::Response::Facets::FacetField.new facet[:id], items
             @facets << new_facet
           end
 
           # CollectionLibrary facet is not very useful; reject it!
-          @facets.reject! {|f| 'CollectionLibrary' == f.name}
+          @facets.reject! { |f| f.name == 'CollectionLibrary' }
 
           # Rearrange to a better order
-          desired_order = [
-            'SourceType',
-            'PublicationYear',
-            'SubjectEDS',
-            'Journal',
-            'Language',
-            'SubjectGeographic',
-            'Publisher',
-            'CollectionLibrary',
-            'ContentProvider'
+          desired_order = %w[
+            SourceType
+            PublicationYear
+            SubjectEDS
+            Journal
+            Language
+            SubjectGeographic
+            Publisher
+            CollectionLibrary
+            ContentProvider
           ]
-          @facets.sort_by! {|f| desired_order.index f.name}
+          @facets.sort_by! { |f| desired_order.index f.name }
         end
       end
+    else
+      redirect_to controller: 'bento', action: 'home'
     end
   end
 
@@ -59,5 +62,4 @@ class ArticlesController < CatalogController
     raw_article = connection.retrieve dbid: params[:db], an: CGI.unescape(params[:id])
     @document = Article.new raw_article
   end
-
 end
