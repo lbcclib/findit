94ad41eb4fb2a97e3c34ce902adfc229bf3dd7fb
Jane Sandberg <sandbej@linnbenton.edu>
2020-08-25 12:39:47 -0700

    Only fetch one cover image per record

--- lib/tasks/data/config/cover_images.macro.rb
+++ lib/tasks/data/config/cover_images.macro.rb
@@ -1,75 +1,71 @@
+# frozen_string_literal: true
+
 require 'net/http'
+
+IDENTIFIER_TYPES_FOR_COVER_IMAGES = {
+  'isbn' => '020',
+  'lccn' => '010',
+  'oclc' => '035'
+}.freeze
+
 module FindIt
   module Macros
+    # Checks OpenLibrary to see if a cover image exists
+    # for a given record, using a variety of identifiers
     module CoverImages
-
       def logger
-        @logger ||= Yell.new(STDERR, :level => "debug")
+        @logger ||= Yell.new(STDERR, level: 'debug')
       end
 
       def cover_image
-        return proc do |record, accumulator|
-          isbns = extract_identifiers '020', record
-          isbns.each do |isbn|
-            if image_exists 'isbn', isbn
-              accumulator << ol_url('isbn', isbn, 'M')
-              self.logger.info("Found image for record #{record['001']} using isbn #{isbn}")
-              break
-            end
-          end
-
-          lccns = extract_identifiers '020', record
-          lccns.each do |lccn|
-            if image_exists 'lccn', lccn
-              accumulator << ol_url('lccn', lccn, 'M')
-              self.logger.info("Found image for record #{record['001']} using lccn #{lccn}")
-              break
-            end
-          end
-
-          oclcs = extract_identifiers '035', record
-          oclcs.each do |oclc|
-            if image_exists 'oclc', oclc
-              accumulator << ol_url('oclc', oclc, 'M')
-              self.logger.info("Found image for record #{record['001']} using oclc num #{oclc}")
+        proc do |record, accumulator|
+          IDENTIFIER_TYPES_FOR_COVER_IMAGES.each do |id_type, tag|
+            image_url = fetch_valid_image_url id_type, tag, record
+            if image_url
+              accumulator << image_url
               break
             end
           end
         end
       end
 
-      def extract_identifiers tag, record
+      def fetch_valid_image_url(id_type, tag, record)
+        identifiers = extract_identifiers tag, record
+        identifiers.each do |identifier|
+          next unless image_exists id_type, identifier
+
+          logger.info("Found image for record #{record['001']} using #{id_type} #{identifier}")
+          return ol_url(id_type, identifier, 'M')
+        end
+        false
+      end
+
+      def extract_identifiers(tag, record)
         identifiers = []
         record.each_by_tag(tag) do |field|
-          subfields = field.find_all do |sf|
-            if (['a', 'z'].include? sf.code)
-              identifiers << sf.value.delete('^0-9')
-            end
+          field.find_all do |sf|
+            identifiers << sf.value.delete('^0-9') if %w[a z].include? sf.code
           end
         end
         identifiers
       end
 
-
-      def ol_url type, identifier, size
+      def ol_url(type, identifier, size)
         "https://covers.openlibrary.org/b/#{type}/#{identifier}-#{size}.jpg?default=false"
       end
 
-      def image_exists type, identifier
+      def image_exists(type, identifier)
         begin
-            # Try fetching a small version of the image to reduce network load
-            response = Net::HTTP.get_response(URI.parse(ol_url type, identifier, 'S'))
-        rescue
-            return false
+          # Try fetching a small version of the image to reduce network load
+          response = Net::HTTP.get_response(URI.parse(ol_url(type, identifier, 'S')))
+        rescue StandardError
+          return false
         end
-        if response.body.include? 'not found'
-            return false
-        elsif ['200', '301', '302'].include? response.code
-            return true
-        end
+        return false if response.body.include? 'not found'
+        return true if %w[200 301 302].include? response.code
+
         false
       end
-
     end
   end
 end
