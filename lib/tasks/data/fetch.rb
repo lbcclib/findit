a5ddc998a3abb5aa5f6be281438912307c4e7f1e
Jane Sandberg <sandbej@linnbenton.edu>
2020-08-27 13:44:31 -0700

    rubocop improvements for fetching metadata

--- lib/tasks/data/fetch.rb
+++ lib/tasks/data/fetch.rb
@@ -1,70 +1,69 @@
+# frozen_string_literal: true
 require 'net/ftp'
 require 'net/http'
 require 'time'
 
 module FindIt
-    module Data
-        module Fetch
-
-            # Fetches a MARC file over http given the config
-            def http config
-                if config['fetch_url'].kind_of? Proc
-                    urls_to_fetch = [config['fetch_url'].call].flatten(1)
-	            else
-                    urls_to_fetch = [config['fetch_url']].flatten(1)
-	            end
-                files_written = []
-                filename = Rails.root.join('lib', 'tasks', 'data', 'new', config['file_prefix'] + '_' + date_downloaded + '.mrc')
-                if File.exist? filename
-                    File.delete filename
-                end
-                urls_to_fetch.each do |url|
-                    uri = URI.parse url
-                    open(filename, 'ab') do |file|
-                        if config.key? 'user'
-                            IO.copy_stream(open(uri, :http_basic_authentication => [config['user'], config['pass']]), file)
-			            else
-                            IO.copy_stream(open(uri, 'rb'), file)
+  module Data
+    # Fetches metadata from external sources
+    module Fetch
+      def http(config)
+        urls_to_fetch = if config['fetch_url'].is_a? Proc
+                          [config['fetch_url'].call].flatten(1)
+                        else
+                          [config['fetch_url']].flatten(1)
                         end
-                    end
-                    files_written << (filename)
-                end
-                return files_written.uniq
+        files_written = []
+        filename = Rails.root.join('lib', 'tasks', 'data', 'new', config['file_prefix'] + '_' + date_downloaded + '.mrc')
+        File.delete filename if File.exist? filename
+        urls_to_fetch.each do |url|
+          uri = URI.parse url
+          open(filename, 'ab') do |file|
+            if config.key? 'user'
+              IO.copy_stream(open(uri, http_basic_authentication: [config['user'], config['pass']]), file)
+            else
+              IO.copy_stream(open(uri, 'rb'), file)
             end
-
-            def ftp config
-                files_written = []
-                ftp = Net::FTP.new config['server']
-	            ftp.passive = true
-                ftp.login config['user'], config['pass']
-                files = ftp.chdir('metacoll/out/ongoing/new')
-                files_written += fetch_latest_files_by_ftp ftp, 'new', config['file_prefix']
-                files = ftp.chdir('../updates')
-                files_written += fetch_latest_files_by_ftp ftp, 'update', config['file_prefix']
-                #files = ftp.chdir('../deletes')
-                #files_written += fetch_latest_files_by_ftp ftp, 'delete', config['file_prefix']
-                ftp.close
-                return files_written
-            end
-
-            private
-            def date_downloaded
-                return DateTime.now.strftime('%F-%H-%M-%S')
-            end
-
-            def fetch_latest_files_by_ftp ftp, directory, prefix
-                files_written = []
-                files = ftp.nlst('*.mrc')
-                files.each do |file|
-                    if (Time.now - (7*24*60*60)) < (ftp.mtime file)
-                        filename = Rails.root.join('lib', 'tasks', 'data', directory, prefix + '_' + date_downloaded + '.mrc').to_s
-                        puts file + ' is saving as ' + filename
-                        ftp.getbinaryfile(file, filename)
-                        files_written << filename
-                    end
-                end
-                return files_written
-            end
+          end
+          files_written << filename
         end
+        files_written.uniq
+      end
+
+      def ftp(config)
+        files_written = []
+        ftp = Net::FTP.new config['server']
+        ftp.passive = true
+        ftp.login config['user'], config['pass']
+        ftp.chdir('metacoll/out/ongoing/new')
+        files_written += fetch_latest_files_by_ftp ftp, 'new', config['file_prefix']
+        ftp.chdir('../updates')
+        files_written += fetch_latest_files_by_ftp ftp, 'update', config['file_prefix']
+        # ftp.chdir('../deletes')
+        # files_written += fetch_latest_files_by_ftp ftp, 'delete', config['file_prefix']
+        ftp.close
+        files_written
+      end
+
+      private
+
+      def date_downloaded
+        DateTime.now.strftime('%F-%H-%M-%S')
+      end
+
+      def fetch_latest_files_by_ftp(ftp, directory, prefix)
+        files_written = []
+        files = ftp.nlst('*.mrc')
+        files.each do |file|
+          next unless (Time.now - (7 * 24 * 60 * 60)) < (ftp.mtime file)
+
+          filename = Rails.root.join('lib', 'tasks', 'data', directory, prefix + '_' + date_downloaded + '.mrc').to_s
+          puts file + ' is saving as ' + filename
+          ftp.getbinaryfile(file, filename)
+          files_written << filename
+        end
+        files_written
+      end
     end
+  end
 end
--- lib/tasks/data/providers.rb
+++ lib/tasks/data/providers.rb
@@ -1,86 +1,89 @@
+# frozen_string_literal: true
+
 require 'dotenv/tasks'
 module FindIt
-    module Data
-        module Providers
-            def find_by_name name
-                record_providers[name]
-            end
+  module Data
+    # This module keeps track of sources for metadata that can be indexed into FindIt
+    module Providers
+      def find_by_name(name)
+        record_providers[name]
+      end
 
-            def all
-                return {
-                    'globe' => {
-                        #This one is fetched manually from http://www.dramaonlinelibrary.com/pages/marc-records,
-        	            #because it's a zip file.  Go to Video Collections > Globe on Screen
-                        'record_provider_facet' => 'Drama Online Library',
-                        'traject_configuration_files' => ['globe.rb', 'proxy.rb',], 
-                     },
-                    'gvrl' => {
-                        #This one is fetched manually from support.gale.com
-                        'record_provider_facet' => 'Gale Virtual Reference Library',
-                        'traject_configuration_files' => ['gvrl.rb',], 
-                        },
-                    'eg' => {
-                        'record_provider_facet' => 'LBCC Evergreen Catalog',
-                        'fetch_method' => :http,
-                        'user' => 'lbcc',
-                        'pass' => ENV['EVERGREEN_PASSWORD'],
-                        'fetch_url' => lambda {
-                                today = DateTime.now
-                                sunday = today - today.wday
-                                prefix = 'https://libweb.cityofalbany.net/filepile/discovery_layer_exports/lbcc_marc_records_items.'
-                                return prefix + sunday.strftime('%F') + '.mrc'
-                            },
-                        'file_prefix' => 'eg_lbcc',
-                        'traject_configuration_files' => ['eg','marc'],
-                        },
-                    'eg_online' => {
-                        'record_provider_facet' => 'LBCC Evergreen Catalog',
-                        'fetch_method' => 'http',
-                        'user' => 'lbcc',
-                        'pass' => ENV['EVERGREEN_PASSWORD'],
-                        'fetch_url' => lambda {
-                                today = DateTime.now
-                                sunday = today - today.wday
-                                prefix = 'https://libweb.cityofalbany.net/filepile/discovery_layer_exports/lbcc_marc_records_uris.'
-                                return prefix + sunday.strftime('%F') + '.mrc'
-                            },
-                        'file_prefix' => 'eg_lbcc',
-                        'traject_configuration_files' => ['eg_lbcc_online.rb', 'link.rb', 'eg_authorities.rb'],
-                        },
-                    'gale' => {
-                        'record_provider_facet' => 'Gale Databases',
-                        'fetch_method' => :http,
-                        'fetch_url' => ['http://access.gale.com/api/dropoff/resources/ovic.mrc',
+      def all
+        {
+          'globe' => {
+            # This one is fetched manually from http://www.dramaonlinelibrary.com/pages/marc-records,
+            # because it's a zip file.  Go to Video Collections > Globe on Screen
+            'record_provider_facet' => 'Drama Online Library',
+            'traject_configuration_files' => ['globe.rb', 'proxy.rb']
+          },
+          'gvrl' => {
+            # This one is fetched manually from support.gale.com
+            'record_provider_facet' => 'Gale Virtual Reference Library',
+            'traject_configuration_files' => ['gvrl.rb']
+          },
+          'eg' => {
+            'record_provider_facet' => 'LBCC Evergreen Catalog',
+            'fetch_method' => :http,
+            'user' => 'lbcc',
+            'pass' => ENV['EVERGREEN_PASSWORD'],
+            'fetch_url' => lambda {
+                             today = DateTime.now
+                             sunday = today - today.wday
+                             prefix = 'https://libweb.cityofalbany.net/filepile/discovery_layer_exports/lbcc_marc_records_items.'
+                             return prefix + sunday.strftime('%F') + '.mrc'
+                           },
+            'file_prefix' => 'eg_lbcc',
+            'traject_configuration_files' => %w[eg marc]
+          },
+          'eg_online' => {
+            'record_provider_facet' => 'LBCC Evergreen Catalog',
+            'fetch_method' => 'http',
+            'user' => 'lbcc',
+            'pass' => ENV['EVERGREEN_PASSWORD'],
+            'fetch_url' => lambda {
+                             today = DateTime.now
+                             sunday = today - today.wday
+                             prefix = 'https://libweb.cityofalbany.net/filepile/discovery_layer_exports/lbcc_marc_records_uris.'
+                             return prefix + sunday.strftime('%F') + '.mrc'
+                           },
+            'file_prefix' => 'eg_lbcc',
+            'traject_configuration_files' => ['eg_lbcc_online.rb', 'link.rb', 'eg_authorities.rb']
+          },
+          'gale' => {
+            'record_provider_facet' => 'Gale Databases',
+            'fetch_method' => :http,
+            'fetch_url' => ['http://access.gale.com/api/dropoff/resources/ovic.mrc',
                             'http://access.gale.com/api/dropoff/resources/uhic.mrc',
                             'http://access.gale.com/api/dropoff/resources/ngma.mrc'],
-                        'file_prefix' => 'gale',
-                        'traject_configuration_files' => ['marc', 'gale', 'proxy'],
-                        },
-                    'jomi' => {
-                        'record_provider_facet' => 'JoMI Surgical Videos',
-                        'fetch_method' => :http,
-                        'fetch_url' => 'https://jomi.com/jomiRecords.mrc',
-                        'file_prefix' => 'jomi',
-                        'traject_configuration_files' => ['marc', 'jomi','proxy'],
-                        },
-                    'oclc' => {
-                        'record_provider_facet' => 'OCLC',
-                        'fetch_method' => :ftp,
-                        'file_prefix' => 'oclc',
-                        'server' => 'ftp2.oclc.org',
-                        'user' => 'olx',
-                        'pass' => ENV['OCLC_PASSWORD'],
-                        'traject_configuration_files' => ['oclc', 'marc'],
-                        },
-                    'opentextbooks' => {
-                        'fetch_url' => 'https://open.umn.edu/opentextbooks/download.marc',
-                        'record_provider_facet' => 'Open Textbook Library',
-                        'traject_configuration_files' => ['marc', 'opentextbooks'],
-                        'file_prefix' => 'umn_otl',
-                        'fetch_method' => :http,
-                        },
-                }
-            end
-        end
+            'file_prefix' => 'gale',
+            'traject_configuration_files' => %w[marc gale proxy]
+          },
+          'jomi' => {
+            'record_provider_facet' => 'JoMI Surgical Videos',
+            'fetch_method' => :http,
+            'fetch_url' => 'https://jomi.com/jomiRecords.mrc',
+            'file_prefix' => 'jomi',
+            'traject_configuration_files' => %w[marc jomi proxy]
+          },
+          'oclc' => {
+            'record_provider_facet' => 'OCLC',
+            'fetch_method' => :ftp,
+            'file_prefix' => 'oclc',
+            'server' => 'ftp2.oclc.org',
+            'user' => 'olx',
+            'pass' => ENV['OCLC_PASSWORD'],
+            'traject_configuration_files' => %w[oclc marc]
+          },
+          'opentextbooks' => {
+            'fetch_url' => 'https://open.umn.edu/opentextbooks/download.marc',
+            'record_provider_facet' => 'Open Textbook Library',
+            'traject_configuration_files' => %w[marc opentextbooks],
+            'file_prefix' => 'umn_otl',
+            'fetch_method' => :http
+          }
+        }
+      end
     end
+  end
 end
