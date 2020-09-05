f191062cea299e0d851ea9cb09525d30920d1cc0
Jane Sandberg <sandbej@linnbenton.edu>
2020-09-04 21:24:33 -0700

    More search relevance tuning

--- spec/solr/relevance_spec.rb
+++ spec/solr/relevance_spec.rb
@@ -31,9 +31,16 @@ RSpec.describe "solr relevance spec" do
     expect(resp).to include(relevant_ids).in_first(3).results
   end
 
+  it 'can do a keyword search including an author and title' do
+    resp = solr_resp_doc_ids_only({'q'=>'Criminal Investigation Lasley'})
+    relevant_id = '343778'
+    expect(resp).to include(relevant_id).as_first
+  end
+
   it 'can do a title search' do
     resp = solr_resp_doc_ids_only({q: 'animal genetics', pf: '${title_pf}', qf: '${title_qf}'})
     relevant_id = 'ocn896839764'
     expect(resp).to include(relevant_id).as_first
   end
+
 end
