496bdd61568de3580fc20fa003a582b21acde203
Jane Sandberg <sandbej@linnbenton.edu>
2020-09-08 09:37:03 -0700

    More solr tuning

--- spec/solr/synonym_spec.rb
+++ spec/solr/synonym_spec.rb
@@ -3,12 +3,19 @@
 RSpec.describe 'solr synonym spec' do
   it 'switches misspelling immingration to immigration' do
     resp = solr_resp_doc_ids_only({ 'q' => 'immingration' })
-    expect(resp).to have_documents
+    relevant_id = '286656'
+    expect(resp).to include(relevant_id).in_first(3).results
   end
 
   it 'knows that college and campus are synonyms' do
     resp = solr_resp_doc_ids_only({ 'q' => 'sexual assault college' })
     relevant_id = '535879'
     expect(resp).to include(relevant_id).as_first
+  end
+
+  it 'delivers good results for query: reproductive freedom for women in the us' do
+    resp = solr_resp_doc_ids_only({ 'q' => 'reproductive freedom for women in the us' })
+    relevant_id = '505189'
+    expect(resp).to include(relevant_id).in_first(2).results
   end
 end
