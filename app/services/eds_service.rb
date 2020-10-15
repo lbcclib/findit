44d089df0e0624cc7d485fff8a1cb0b72c78eeca
Jane Sandberg <sandbej@linnbenton.edu>
2020-10-15 14:07:53 -0700

    Some minor corrections to the EDS service

--- app/services/eds_service.rb
+++ app/services/eds_service.rb
@@ -10,12 +10,16 @@ class EdsService
 
   def self.blacklight_style_search(params)
     connection = connect
-    connection.search eds_style(params.to_unsafe_h), false, false
+    permitted_params = params.permit(:q, :page, :search_field, f: {})
+    connection.search eds_style(permitted_params.to_h), false, false
   end
 
   def self.eds_style(params)
     # EDS gem doesn't understand the all_fields search_field
-    params['search_field'] = 'KW' if (params.key? 'search_field') && (params['search_field'] == 'all_fields')
+    params['search_field'] = 'KW' if params['search_field'] == 'all_fields'
+
+    # EDS defaults to 20 pages, which is more than we want
+    params['results_per_page'] ||= 10
     params.with_indifferent_access
   end
 end
