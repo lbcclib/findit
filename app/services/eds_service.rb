c2b8c742a72df3b4205b8cd500e6eb7ba827467a
Jane Sandberg <sandbej@linnbenton.edu>
2020-11-06 07:46:37 -0800

    Log the eds session

--- app/services/eds_service.rb
+++ app/services/eds_service.rb
@@ -3,9 +3,11 @@
 # Service that gets article metadata and searches
 class EdsService
   def self.connect
-    Rails.cache.fetch('eds-session', expires_in: 12.minutes) do
+    ses = Rails.cache.fetch('eds-session', expires_in: 12.minutes) do
       ::EBSCO::EDS::Session.new max_attempts: 10
     end
+    Rails.logger.info ses.inspect
+    ses
   end
 
   def self.blacklight_style_search(params)
