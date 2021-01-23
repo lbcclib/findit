72e53790769ad8c59d066ea6a404f5babb09c7a9
Jane Sandberg <sandbej@linnbenton.edu>
2021-01-23 08:59:39 -0800

    Refactor EDS service
    So there's no potential for infinite loops!  Also it's DRYer.

--- app/services/eds_service.rb
+++ app/services/eds_service.rb
@@ -2,26 +2,20 @@
 
 # Service that gets article metadata and searches
 class EdsService
-  def self.connect(force_new_session = false)
+  def self.connect(force_new_session: false)
     ses = Rails.cache.fetch('eds-session', expires_in: 12.minutes, force: force_new_session) do
-      ::EBSCO::EDS::Session.new max_attempts: 10
+      ::EBSCO::EDS::Session.new max_attempts: 2
     end
     Rails.logger.info ses.inspect
     ses
   end
 
-  def self.search(params, force_new_session = false)
-    connection = connect
-    connection.search params, false, false
-  rescue EBSCO::EDS::ApiError, EBSCO::EDS::BadRequest
-    self.search params, true
+  def self.search(params)
+    send_to_eds { |connection| connection.search params, false, false }
   end
 
-  def self.retrieve(dbid, an, force_new_session = false)
-    connection = connect
-    connection.retrieve dbid: dbid, an: an
-  rescue EBSCO::EDS::ApiError, EBSCO::EDS::BadRequest
-    self.retrieve dbid, an
+  def self.retrieve(database_id, accession_number)
+    send_to_eds { |connection| connection.retrieve dbid: database_id, an: accession_number }
   end
 
   def self.blacklight_style_search(params)
@@ -38,5 +32,13 @@ class EdsService
 
   def self.safe_params
     [:q, :page, :search_field, :db, :highlight, :view, { range: {}, f: {} }]
+  end
+
+  def self.send_to_eds
+    connection = connect
+    yield connection
+  rescue EBSCO::EDS::ApiError, EBSCO::EDS::BadRequest
+    connection = connect force_new_session: true
+    yield connection
   end
 end
