109e27aa05120b7d1aaf5bcc27c4ce80db9de315
Jane Sandberg <sandbej@linnbenton.edu>
2021-02-04 17:19:12 -0800

    Keeping rubocop happy

--- app/components/bento/catalog_facet_component.rb
+++ app/components/bento/catalog_facet_component.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# Display a fake "facet" in the bento views
 module Bento
+  # Display a fake "facet" in the bento views
   class CatalogFacetComponent < ViewComponent::Base
     def initialize(facet_key:, label:, catalog_format_facets:)
-        @facet_key = facet_key
-        @label = label
-        @catalog_format_facets = catalog_format_facets
-        super
+      @facet_key = facet_key
+      @label = label
+      @catalog_format_facets = catalog_format_facets
+      super
     end
   end
 end
