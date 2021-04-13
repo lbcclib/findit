ccf3ecbe8235ba692f43f16c8fb1e8ae14fa378b
Jane Sandberg <sandbej@linnbenton.edu>
2021-04-13 06:03:05 -0700

    No need for custom URL handling in OCLC indexing config, closes #421

--- lib/tasks/data/config/oclc.rb
+++ lib/tasks/data/config/oclc.rb
@@ -5,12 +5,6 @@ require_relative 'cover_images.macro'
 extend FindIt::Macros::CoverImages
 require_relative 'oclc_format.macro'
 
-needs_proxy = [
-  'American History in Video United States',
-  'Music Online: Classical Music Library - United States',
-  'Music Online: Smithsonian Global Sound for Libraries'
-]
-
 to_field 'thumbnail_path_ss', cover_image
 
 to_field 'id', extract_marc('001', first: true)
@@ -21,17 +15,4 @@ to_field 'is_electronic_facet', literal('Online')
 
 to_field 'format', FindIt::Macros::OCLCFormat.assign_format, default('Ebook')
 
-to_field 'url_fulltext_display' do |record, accumulator|
-  db = record['950']['a'].to_s
-  urls = record.find_all { |f| f.tag == '856' }
-  urls.each do |field|
-    value = field['u']
-    accumulator << if needs_proxy.include? db
-                     "http://ezproxy.libweb.linnbenton.edu:2048/login?url=#{value}"
-                   elsif db == 'Ebook Central Academic Complete'
-                     value.sub('lib//detail.action', 'lib/linnbenton-ebooks/detail.action')
-                   else
-                     value
-                   end
-  end
-end
+to_field 'url_fulltext_display', extract_marc('856|40|u')
